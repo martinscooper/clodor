bc199557c4aaa9801a7c897f313d3061b8464e18
Martín Santillán Cooper <msantillancooper@ibm.com>
2023-12-19 16:06:49 -0300

    new

--- myscript.js
+++ myscript.js
@@ -6,12 +6,12 @@ const createItem = (name, desc, img) => {
   const innerH3 = document.createElement("h5");
   innerH3.className = "text-center card-title ";
   innerH3.textContent = name;
-  innerH3.style = "font-style: italic"
+  innerH3.style = "font-style: italic";
 
   const innerP = document.createElement("p");
   innerP.innerHTML = desc;
   innerP.className = "text-center lead card-text";
-  innerP.style = ""
+  innerP.style = "";
 
   const contentDiv = document.createElement("div");
   contentDiv.className = "card-body";
@@ -26,7 +26,7 @@ const createItem = (name, desc, img) => {
   const divOuter = document.createElement("div");
   divOuter.className = "col-6 col-sm-4";
   divOuter.appendChild(divInner);
-  divOuter.style="display: flex; align-self: stretch;"
+  divOuter.style = "display: flex; align-self: stretch;";
 
   return divOuter;
 };
@@ -62,11 +62,11 @@ const desc = [
   "de boldo o manzanilla",
   "o té de peperina",
 
-  "a mi girame para mirar al sol",
+  "girame para mirar al sol",
   "o poneme de espaldas a la noche",
   "o lo que sientas",
 
-  "a mi googleame por rara",
+  "googleame por rara",
   "solo vivo si",
   "me dejan morir",
 
@@ -85,11 +85,26 @@ const desc = [
   "y que Dios sea",
   "como siempre",
   "un deseo olvidado",
-]
+];
 
 const descriptions = [];
 
 new Array(21).fill(32).forEach((_, i) => {
   const item = createItem(names[i], desc[i], `assets/plant${i + 1}.jpg`);
   list.appendChild(item);
+});
+
+const poem = document.getElementById("poem");
+poem.style = "margin-top: 30px;margin-bottom: 30px;"
+const poemTitle = document.createElement("h4");
+poemTitle.innerHTML = "Instrucciones para regar un dpto";
+poemTitle.className = "pb-2"
+poem.appendChild(poemTitle);
+
+new Array(21).fill(32).forEach((_, i) => {
+  const line = document.createElement("h6");
+  line.innerHTML = desc[i];
+  line.className = "text-muted";
+  line.className = `${line.className}`;
+  poem.appendChild(line);
 });
